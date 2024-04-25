a9f15aff37141fda57ba65e28bd3c0ffe906de86
David Gelpi <gelpiorama@gmail.com>
2024-04-25 13:09:00 +0200

    build(pom.xml): Corregir clase principal de la app

--- src/test/java/org/foobarspam/MockitoEjemplo/CarreraTest.java
+++ src/test/java/org/foobarspam/MockitoEjemplo/CarreraTest.java
@@ -4,8 +4,6 @@ import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-import java.util.ArrayList;
-
 import org.junit.Before;
 import org.junit.Test;
 
